bda1ecf24d6e5f0776bd290348eb0a03b62be687
Vineeth N Krishnan <vineeth@soarmorrow.com>
2018-09-12 14:23:57 +0530

    Initial Commit

---
+++ src/Facades/VisitLog.php
@@ -0,0 +1,18 @@
+<?php
+namespace Vineethkrishnan\VisitLog\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class VisitLog extends Facade
+{
+    /**
+     * Get the binding in the IoC container
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'VisitLog';
+    }
+
+} 
